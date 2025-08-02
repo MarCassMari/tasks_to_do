83841f34d7e4eff0d0d697e58ae0f013e48ad6a9
MarCassMari <marcassmachado32@gmail.com>
2025-08-02 11:36:42 -0300

    feature:rota de pegar detalhes de uma tarefa apenas

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 
 const connectToDatabase = require("./src/database/mongoose.database");
 const TaskModel = require("./src/models/task.model");
+const e = require("express");
 
 dotenv.config();
 const app = express();
@@ -39,15 +40,29 @@ app.post("/tasks", async (req, res) => {
 });
 
 app.delete("/tasks/:id", async (req, res) => {
-    const taskId = req.params.id;
-    const taskExists = await TaskModel.findById(taskId);
-
-    if (!taskExists) {
-        return res.status(500).send({ error: "Tarefa não encontrada!" });
-    }
     try {
+        const taskId = req.params.id;
+        const taskExists = await TaskModel.findById(taskId);
+        if (!taskExists) {
+            return res.status(404).send({ error: "Tarefa não encontrada!" });
+        }
         const deletedTask = await TaskModel.findByIdAndDelete(taskId);
         res.status(200).send(deletedTask);
+    } catch (error) {
+        res.status(500).send();
+    }
+});
+
+app.get("/tasks/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+        const task = await TaskModel.findById(taskId);
+        if (!task) {
+            return res.status(404).send({
+                error: "Tarefa não foi encontrada no banco de dados!",
+            });
+        }
+        res.status(200).send(task);
     } catch (error) {
         res.status(500).send();
     }
