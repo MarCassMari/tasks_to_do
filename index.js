686c8cebac09e45b011b8b8818b2585988dd02e3
MarCassMari <marcassmachado32@gmail.com>
2025-09-03 20:29:58 -0300

    feat: cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const TaskRoutes = require("./src/routes/task.routes");
+const cors = require("cors");
 
 const connectToDatabase = require("./src/database/mongoose.database");
 const TaskModel = require("./src/models/task.model");
@@ -8,6 +9,7 @@ const e = require("express");
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 connectToDatabase();
